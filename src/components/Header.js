3880588f897aa3889bf74c37a59a5b6c61157c0f
superzaky <superkytoz@gmail.com>
2021-05-26 12:53:55 +0200

    add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Typed from "react-typed";
 import logo from "../logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
     return (
@@ -19,15 +21,13 @@ const Header = () => {
                                     loop
                                 />
                             </div>
-                            <div class="home__social">
-                                <a href="" class="home__social-icon"><i class='bx bxl-linkedin'></i>asd</a>
-                                <a href="" class="home__social-icon"><i class='bx bxl-behance' ></i>asd</a>
-                                <a href="" class="home__social-icon"><i class='bx bxl-github' ></i>asd</a>
+                            <div class="home_social">
+                                <a href="https://github.com/superzaky" target="_blank" class="home_social-icon"><FontAwesomeIcon icon={faGithub} /></a>
+                                <a href="https://twitter.com/zakyhuraibi" target="_blank" class="home_social-icon"><FontAwesomeIcon icon={faTwitter} /></a>
+                                <a href="https://medium.com/@zakyhuraibi" target="_blank" class="home_social-icon"><FontAwesomeIcon icon={faMedium} /></a>
                             </div>
                         </div>
-
                         <div class="col-lg-1">
-                          
                         </div>
                         <div class="col-lg-6">
                             <div className="header-image">
