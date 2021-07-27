a3455b75452571fee0419c272afd0f9c9283e1cb
superzaky <superkytoz@gmail.com>
2021-07-28 00:11:36 +0200

    edit layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,10 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="footer_title">Zaky Huraibi</p>
-            <div className="footer_social">
-                <a href="#" className="footer_icon"><i className='bx bxl-facebook' ></i></a>
-                <a href="#" className="footer_icon"><i className='bx bxl-instagram' ></i></a>
-                <a href="#" className="footer_icon"><i className='bx bxl-twitter' ></i></a>
+            <div className="container">
+                <p className="footer_title">Zaky Huraibi</p>
+                <p>&#169; 2021 copyright all rights reserved</p>
             </div>
-            <p>&#169; 2021 copyright all rights reserved</p>
         </footer>
     )
 }
